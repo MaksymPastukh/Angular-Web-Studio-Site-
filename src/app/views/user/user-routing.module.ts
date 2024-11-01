3ee2bfa5edea1feb95d1a10d4a700398d7cb40e7
MaksymPastukh <saponexamq@gmail.com>
2024-11-01 18:55:08 +0100

    Added privacy policy page

--- src/app/views/user/user-routing.module.ts
+++ src/app/views/user/user-routing.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import {LoginComponent} from "./login/login.component";
-import {SignupComponent} from "./signup/signup.component";
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { LoginComponent } from "./login/login.component"
+import { SignupComponent } from "./signup/signup.component"
+import { UserAgreementComponent } from './user-agreement/user-agreement.component'
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent},
-  {path: 'signup', component: SignupComponent}
-];
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'agreement', component: UserAgreementComponent },
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/views/user/user.module.ts
+++ src/app/views/user/user.module.ts
@@ -5,12 +5,14 @@ import { UserRoutingModule } from './user-routing.module';
 import {SignupComponent} from "./signup/signup.component";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {LoginComponent} from "./login/login.component";
+import { UserAgreementComponent } from './user-agreement/user-agreement.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
     SignupComponent,
+    UserAgreementComponent,
   ],
   imports: [
     CommonModule,
